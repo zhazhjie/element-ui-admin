7da4234a8a3b291ba7dbf40a0e821da508c042ff
zhazhjie <583714409@qq.com>
2019-09-09 14:49:30 +0800

    init

---
+++ web-admin/admin/src/main/java/com/web/admin/modules/sys/controller/BaseController.java
@@ -0,0 +1,11 @@
+package com.web.admin.modules.sys.controller;
+
+import com.web.admin.modules.sys.entity.po.SysUser;
+
+public class BaseController {
+    public SysUser getUser(){
+        SysUser sysUser = new SysUser();
+        sysUser.setId(1156876219089100802L);
+        return sysUser;
+    }
+}
