de488e174f317f2b099d0c0074d143f24ccb777e
zhazhjie <583714409@qq.com>
2019-09-20 15:17:59 +0800

    init

--- src/store/index.js
+++ src/store/index.js
@@ -2,80 +2,76 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import {getUserInfo} from '../api/sys/user'
 import {listUserPermission} from '../api/sys/permission'
-import {setMenu,getStore,setStore} from '../utils/util'
+import {setMenu, getStore, setStore} from '../utils/util'
 import {listToMap, toTreeData, treeDataTranslate} from "../utils/util";
 import staticRouter from '../router/staticRouter'
+
 Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
-    userInfo:{},
-    menuList:[],
-    permissions:[],
-    size:getStore('size')||'mini',
-    isCollapse:false,
-    menuInitialized:false,
-    cacheMenuList:getStore("cacheMenuList")||[]
+    userInfo: {},
+    menuList: [],
+    permissions: [],
+    size: getStore('size') || 'mini',
+    isCollapse: false,
+    menuInitialized: false,
+    cacheMenuList: getStore("cacheMenuList") || []
   },
-  actions:{
-    getUserInfo({commit,state,dispatch}){
+  actions: {
+    getUserInfo({commit, state, dispatch}) {
       return new Promise((resolve, reject) => {
-          getUserInfo().then(res=>{
-            setStore('userInfo',res.data||null);
-            state.userInfo=res.data||{};
-            resolve(res.data);
-          })
-            .catch(err=>reject(err))
-      }).catch(err=>reject(err))
+        getUserInfo().then(res => {
+          setStore('userInfo', res.data || null);
+          state.userInfo = res.data || {};
+          resolve(res.data);
+        })
+          .catch(err => reject(err))
+      }).catch(err => reject(err))
     },
     listUserPermission: function ({commit, state, dispatch}) {
       return new Promise((resolve, reject) => {
         listUserPermission().then(res => {
-          // setStore('menuList', res.data.menuList || null);
           state.menuList = setMenu(treeDataTranslate(res.data.menuList)).concat(staticRouter);
-          // setStore('permissions', res.data.permissions || null);
           state.permissions = res.data.permissions || [];
           state.menuInitialized = true;
           resolve();
         })
-          .catch(err=>reject(err))
-      }).catch(err=>reject(err))
+          .catch(err => reject(err))
+      }).catch(err => reject(err))
     },
   },
-  getters:{
-
-  },
+  getters: {},
   mutations: {
-    setSize(state,size){
-      state.size=size;
-      setStore('size',size);
+    setSize(state, size) {
+      state.size = size;
+      setStore('size', size);
     },
-    setCollapse(state,isCollapse){
-      state.isCollapse=!state.isCollapse;
+    setCollapse(state) {
+      state.isCollapse = !state.isCollapse;
     },
-    logout(state){
-        // setStore('menuList',null);
-        // setStore('permissions',null);
-        // setStore('userInfo',null);
-        setStore('token',null,'local');
-        state.menuList=[];
-        state.permissions=[];
-        state.userInfo={};
-        location.reload();
+    logout(state) {
+      setStore('token', null, 'local');
+      setStore('cacheMenuList', null);
+      state.menuList = [];
+      state.permissions = [];
+      state.userInfo = {};
+      state.cacheMenuList = [];
+      location.reload();
     },
-    pushCacheMenu(state,data){
-      let isExist=state.cacheMenuList.some(v=>v.path===data.path);
-      if(isExist) return;
+    pushCacheMenu(state, data) {
+      let isExist = state.cacheMenuList.some(v => v.path === data.path);
+      if (isExist) return;
       state.cacheMenuList.push(data);
-      setStore('cacheMenuList',state.cacheMenuList);
+      setStore('cacheMenuList', state.cacheMenuList);
     },
-    removeCacheMenu(state,data){
-      let cacheMenuList=state.cacheMenuList.filter(v=>v.path!==data.path);
-      state.cacheMenuList=cacheMenuList;
-      setStore('cacheMenuList',state.cacheMenuList);
+    removeCacheMenu(state, data) {
+      let cacheMenuList = state.cacheMenuList.filter(v => v.path !== data.path);
+      state.cacheMenuList = cacheMenuList;
+      setStore('cacheMenuList', state.cacheMenuList);
     },
-    removeAllCacheMenu(state){
-      state.cacheMenuList=[];
-      setStore('cacheMenuList',null);
+    removeAllCacheMenu(state) {
+      state.cacheMenuList = [];
+      setStore('cacheMenuList', null);
     },
   }
 })
