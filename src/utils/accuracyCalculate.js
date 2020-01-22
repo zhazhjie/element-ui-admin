d0d9d46f058d788dd54d0a5ccba3b5509c91e25a
zhazhjie <583714409@qq.com>
2020-01-22 15:50:55 +0800

    添加格式化数值

--- src/utils/accuracyCalculate.js
+++ src/utils/accuracyCalculate.js
@@ -6,12 +6,30 @@
  * 修复如：0.1+0.2，0.58*100等精度问题，toFixed bug
  */
 
+/**
+ * 获取小数位长度
+ * @param num
+ * @returns {number}
+ */
 let getDecimalLen = num => ((num + "").split(".")[1] || "").length;
-
+/**
+ * 去小数点：小数转整数
+ * @param num
+ * @returns {number}
+ */
 let decimalToInteger = num => +(num + "").replace(/^0?\.0*|\./g, "");
-
+/**
+ * 10的次幂
+ * @param index
+ * @returns {number}
+ */
 let powerTen = index => Math.pow(10, index);
-
+/**
+ * 加法：支持多个数相加
+ * 如：0.1.add(0.2,0.3)
+ * @param args
+ * @returns {number}
+ */
 Number.prototype.add = function (...args) {
   let allArgs = [this.valueOf(), ...args];
   let maxDecimalLen = Math.max(...allArgs.map(v => getDecimalLen(v)));
@@ -21,12 +39,20 @@ Number.prototype.add = function (...args) {
     return total + decimalToInteger(cur) * powerTen(diffDecimalLen);
   }, 0) / powerTen(maxDecimalLen);
 };
-
+/**
+ * 减法，同上
+ * @param args
+ * @returns {number}
+ */
 Number.prototype.subtract = function (...args) {
   args = args.map(v => -v);
   return this.add(...args);
 };
-
+/**
+ * 乘法，同上
+ * @param args
+ * @returns {any | number}
+ */
 Number.prototype.multiply = function (...args) {
   let allArgs = [this.valueOf(), ...args];
   return allArgs.reduce((total, cur) => {
@@ -35,7 +61,11 @@ Number.prototype.multiply = function (...args) {
     return decimalToInteger(total) * decimalToInteger(cur) / powerTen(curDecimalLen + totalDecimalLen);
   });
 };
-
+/**
+ * 除法，同上
+ * @param args
+ * @returns {any}
+ */
 Number.prototype.divide = function (...args) {
   let allArgs = [this.valueOf(), ...args];
   return allArgs.reduce((total, cur) => {
@@ -64,28 +94,24 @@ Number.prototype.$toFixed = function (fractionDigits) {
   return result[0] + (result[1] ? "." + result[1].padEnd(fractionDigits, "0") : "");
 };
 
-function roundingDown(num, fractionDigits) {
+/**
+ * 取整
+ * @param num
+ * @param fractionDigits
+ * @param down
+ * @returns {string}
+ */
+function rounding(num, fractionDigits, down) {
   let decimalLen = getDecimalLen(num);
   if (decimalLen <= fractionDigits) {
     return num.$toFixed(fractionDigits);
   } else {
-    num += "";
-    num = num.substr(0, num.length - (decimalLen - fractionDigits));
-    return (+num).$toFixed(fractionDigits);
-  }
-}
-
-function roundingUp(num, fractionDigits) {
-  let decimalLen = getDecimalLen(num);
-  if (decimalLen <= fractionDigits) {
-    return num.$toFixed(fractionDigits);
-  } else {
-    num += "";
     let len = decimalLen - fractionDigits;
     let intVal = decimalToInteger(num);
-    intVal += powerTen(len);
+    let diff = powerTen(len) >> 1;
+    intVal = down ? intVal - diff : intVal + diff;
     num = intVal / powerTen(decimalLen);
-    return roundingDown(num, fractionDigits)
+    return num.$toFixed(fractionDigits);
   }
 }
 
@@ -101,8 +127,8 @@ Number.prototype.setScale = function (fractionDigits = 2, roundingMode = 0) {
     case 0:
       return this.$toFixed(fractionDigits);
     case 1:
-      return roundingUp(num, fractionDigits);
+      return rounding(num, fractionDigits);
     case 2:
-      return roundingDown(num, fractionDigits);
+      return rounding(num, fractionDigits, true);
   }
 };
